1ac0c52b5805810753ac29c51af1ce695cb3ffa8
maybeAfnan <afnan.75@outlook.sa>
2025-11-03 17:42:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
-
-
 const API_BASE = 'https://www.themealdb.com/api/json/v1/1/';
 const container = document.getElementById('recipes-container');
 const categoryButtons = document.querySelectorAll('.category-title');
 
+const MEAL_DATA = [
+    { mealId: '53076', calories: 780, cookingTime: 40 },
+    { mealId: '52965', calories: 950, cookingTime: 55 },
+    { mealId: '52895', calories: 410, cookingTime: 25 },
+    { mealId: '52957', calories: 1100, cookingTime: 75 },
+    { mealId: '52896', calories: 630, cookingTime: 30 },
+    { mealId: '52967', calories: 340, cookingTime: 15 },
+    { mealId: '52842', calories: 480, cookingTime: 22 },
+    { mealId: '52840', calories: 710, cookingTime: 45 },
+    { mealId: '52779', calories: 880, cookingTime: 60 },
+    { mealId: '52841', calories: 590, cookingTime: 35 },
+    { mealId: '53099', calories: 390, cookingTime: 18 },
+    { mealId: '52874', calories: 750, cookingTime: 48 },
+    { mealId: '52878', calories: 920, cookingTime: 65 },
+    { mealId: '53071', calories: 510, cookingTime: 28 },
+    { mealId: '52997', calories: 1050, cookingTime: 80 },
+    { mealId: '52904', calories: 430, cookingTime: 20 },
+    { mealId: '53085', calories: 680, cookingTime: 42 },
+    { mealId: '53050', calories: 810, cookingTime: 55 },
+    { mealId: '52940', calories: 360, cookingTime: 17 },
+    { mealId: '53016', calories: 980, cookingTime: 70 },
+    { mealId: '52846', calories: 490, cookingTime: 24 },
+    { mealId: '52796', calories: 730, cookingTime: 49 },
+    { mealId: '52807', calories: 560, cookingTime: 33 },
+    { mealId: '53078', calories: 1150, cookingTime: 90 },
+    { mealId: '53077', calories: 460, cookingTime: 25 },
+    { mealId: '52870', calories: 840, cookingTime: 58 },
+    { mealId: '53072', calories: 600, cookingTime: 38 },
+    { mealId: '52785', calories: 1020, cookingTime: 85 },
+    { mealId: '53049', calories: 380, cookingTime: 19 },
+    { mealId: '52893', calories: 770, cookingTime: 52 },
+    { mealId: '52768', calories: 530, cookingTime: 30 },
+    { mealId: '52767', calories: 900, cookingTime: 62 },
+    { mealId: '52855', calories: 450, cookingTime: 21 },
+    { mealId: '52894', calories: 690, cookingTime: 44 }
+];
+
+const dataCache = MEAL_DATA.reduce((acc, item) => {
+    acc[item.mealId] = {
+        calories: item.calories,
+        cookingTime: item.cookingTime,
+    };
+    return acc;
+}, {});
 
 categoryButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -46,16 +88,21 @@ function renderMeals(meals) {
 
     container.innerHTML = '';
     
-    const displayMeals = meals.slice(0, 7); 
+    const displayMeals = meals.slice(0, 6); 
 
     displayMeals.forEach(meal => {
         const card = document.createElement('div');
         card.className = 'recipe-card';
+        const mealsData = dataCache[meal.idMeal] || { cookingTime: 'N/A', calories: 'N/A' };
    
         const recipeHtml = `
             <img src="${meal.strMealThumb}" alt="${meal.strMeal}" class="card-image-placeholder">
             <div class="card-body">
                 <h3 class="card-title">${meal.strMeal}</h3> 
+                <div>
+                <p class = "card-meta">
+                    Time: ${mealsData.cookingTime} min - Calories: ${mealsData.calories} cal</p>
+                </div>
                 <div class="card-btns">
                     <button class="details-button" data-id="${meal.idMeal}">View Details</button>
                     <button class="add-favorites-button">Add to Favorites</button>
